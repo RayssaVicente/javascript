447bbc2109ab28061173c137750a5c7474bb5ef3
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2023-12-05 09:54:29 -0300

    Estudando um pouco de Javascript

---
+++ objetos/index.js
@@ -0,0 +1,63 @@
+/*let nome = "prop"
+
+nome = "prop"
+
+let objeto = {}
+
+objeto.prop = "Olá mundo!"
+
+objeto
+
+(prop: 'Olá mundo!')
+
+objeto.prop
+
+Olá mundo!
+
+objeto[nome]
+
+'Olá mundo!'
+
+objeto.prop === objeto["prop"]
+true
+
+objeto.prop === objeto[nome]
+true
+
+objeto.prop === objeto["pro" + "p"]
+true
+
+
+let funcao = "log"
+
+funcao
+'log'
+
+console[funcao]("Olá mundo!")
+
+const pessoa = {}
+
+pessoa.nome = "isaac"
+
+pessoa.idade = 26
+
+pessoa 
+(nome: 'isaac', idade: 26
+    idade: 26
+    nome: 'isaac'
+)
+
+pessoa.colegas = ["lucas", "juliana", "Rafael"]
+
+pessoa.endereco = {
+    rua: "logo ali"
+    numero:  42
+    bairro: "centro"
+}
+
+pessoa (Pucharia, o nome, idade, colegas e endereco colocados ate agora)
+
+*/
+
+
+
