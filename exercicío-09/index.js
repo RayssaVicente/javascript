75bda98e3189ed921f01b94942b72bbb1fa1fa60
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2023-12-12 07:09:59 -0300

    Estudando um pouco javascript

---
+++ exercicío-09/index.js
@@ -0,0 +1,24 @@
+/*Calculadora Geométrica
+
+Escreva um programa em javascript para calcular a área de diferentes formas geométricas. 
+
+O programa deve iniciar com um menu contendo as diferentes opções de cálculas. As opções devem ser:
+área do triângulo: base * altura / 2
+área do retângulo: base * altura
+área do quadrado: lado²
+área do trapézio: (base maior + base menor) * altura / 2
+área do círculo: pi * raio² (considere pi = 3.14)
+
+Você deve escrever o programa usando funções sempre que possível para separar os procedimentos. O programa também deve ter uma opção de “Sair” e enquanto ela não for escolhida deverá voltar ao menu.*/
+
+let opcao = ""
+
+do{
+    opcao = prompt ("Bem-vindo(a) a Calculadora geometrica\n\n1- Área do triângulo\n2- Área do retângulo\n3- Área do quadrado\n4- Área do trapézio\n5- Área do círculo\n6- Sair" )
+
+    switch(opcao){
+        case "1":
+            
+    }
+
+}while(opcao !== "6")
--- exercício-08/index.js
+++ exercício-08/index.js
@@ -18,4 +18,60 @@ Quantidade de banheiros.
 Se possui garagem.
 O menu também deve ter a opção de mostrar todos os imóveis salvos.*/
 
+const imoveis = []
+let opcao = ""
 
+
+
+do{
+    
+    opcao =prompt("imóveis cadastrados: " + imoveis.length + "\n1 - Salvar um imovel\n2 - Mostra todos os imoveis\n3 - Sair" )
+
+    switch(opcao){
+        case "1":
+            const imovel = {}
+
+            imovel.proprietario = prompt("Qual é o nome do proprietário? ")
+           
+            imovel.quantidadeQuartos = prompt("Quantos quartos tem o imovel? ")
+           
+            imovel.quantidadeBanheiros = prompt("quantos banheiros tem o imovel? ")
+           
+            imovel.garagem = prompt ("O imovel tem garagem: (sim/ não)")
+            
+            const confirmar = confirm(
+                "Salvar este imóvel?\n" +
+                "\nProprietário: " + imovel.proprietario +
+                "\nQuartos: " + imovel.quantidadeBanheiros +
+                "\nBanheiro: " + imovel.quantidadeBanheiros +
+                "\nPossui Garagem? " + imovel.garagem
+            )
+
+            if(confirmar){
+                imoveis.push(imovel)
+            }
+
+           break
+        case "2":
+            for (let i = 0; i < imoveis.length; i++){
+                alert(
+                    "Imóvel "+ (i + 1) +
+                    "\nProprietário: " + imoveis[i].proprietario +
+                    "\nQuartos: " + imoveis[i].quantidadeQuartos +
+                    "\nBanheiros: " + imoveis[i].quantidadeBanheiros +
+                    "\nPossui Garagem? " + imoveis[i].garagem
+                )
+            }
+            
+            break
+        case "3":
+            alert("Saindo...")
+            break
+        default:
+            alert("Opção Invalida!")
+            break
+    }
+
+}while(opcao !== "3"){
+    
+}
