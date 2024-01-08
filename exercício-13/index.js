f648e1122bde3c68deacc7e246470bc17acaf577
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-08 09:19:49 -0300

    Exercício-13 - continuação

--- exercício-13/index.js
+++ exercício-13/index.js
@@ -10,12 +10,14 @@ Construa um jogo tic-tac-toe, conhecido em português como jogo da velha, utiliz
 - Em caso de empate, uma mensagem de empate deve ser mostrada na tela;
 - Deve ser possível reiniciar o jogo para jogar novamente.*/
 
+
+
 const boardRegions =  document.querySelectorAll('#gameBoard span')
 let vBoard = []
 let turnPlayer = ''
 
-function updatetitle (){
-    const playerInput = document.getElementById('turnPlayer')
+function updateTitle (){
+    const playerInput = document.getElementById(turnPlayer)
     document.getElementById('turnPlayer').innerText = playerInput.value
 }
 
@@ -23,7 +25,7 @@ function initializeGame(){
     vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
     turnPlayer = 'player1'
     document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
-    updatetitle()
+    updateTitle()
     boardRegions.forEach(function (element){
         element.classList.remove('win')
         element.innerText = ''
@@ -31,8 +33,29 @@ function initializeGame(){
     })
 }
 
-function handleBoardClick(ev){
-
+function getWinRigions(){
+    const winRegions = []
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+    winRegions.push("0.0", "0.1", "0.2")
+    if(vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+    winRegions.push("1.0", "1.1", "1.2")
+    if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+    winRegions.push("2.0", "2.1", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+    winRegions.push("0.0", "1.0", "2.0")
+    if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+    winRegions.push("0.1", "1.1", "2.1")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+    winRegions.push("0.2", "1.2", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+    winRegions.push("0.0", "1.1", "2.2")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+    winRegions.push("0.2", "1.1", "2.0")
+    return winRegions
 }
 
-// document.getElementById('start').addEventListener('click', initializeGame)
+function disableRegion(element){
+    element.style.cursor = 'default'
+    element.removeEventListener('click', handleBoardClick)
+}
+
