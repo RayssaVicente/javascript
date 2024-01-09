6b2e051f285881c560184993dba4b958b00b5b4a
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-09 09:27:25 -0300

    Continuação

--- 94-Arrow Function/index.js
+++ 94-Arrow Function/index.js
@@ -19,3 +19,12 @@ console.log(`Soma arrow function: ${arrowSum(6, 6)}`)
 const subtact = (a, b) => a - b
 
 console.log(`Subtração: ${subtact(5, 2)}`)
+
+const double = n => `O dobro de ${n} é ${n * 2}`
+const number = 21
+console.log(double(number))
+
+
+const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffe']
+const startingWithP = towns.filter(towns => towns[0] === 'P')
+console.log(startingWithP)
