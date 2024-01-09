d9f6a018584e5e995a4ff6d0299040c2e90fa656
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-09 10:19:36 -0300

    Operadores Spread

---
+++ 97-Operador-spread/index.js
@@ -0,0 +1,7 @@
+const tows = ['Protera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Juno']
+
+console.log(tows)
+console.log(...tows)
+console.log(...tows[0]) // conole.log(P, r, o, n, t, e, r, a)
+
+const towsCopy = tows
