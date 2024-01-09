afbe3b0afe19e434b14c94c3e63c00fa75eca44d
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-09 10:52:29 -0300

    Atualização

--- 97-Operador-spread/index.js
+++ 97-Operador-spread/index.js
@@ -5,3 +5,20 @@ console.log(...tows)
 console.log(...tows[0]) // conole.log(P, r, o, n, t, e, r, a)
 
 const towsCopy = tows
+
+towsCopy.pop()
+towsCopy.pop()
+tows.push('Marcher')
+
+console.log({tows, towsCopy})
+
+const towsClone = [...tows]
+towsClone.push('Aldebaran')
+console.log({tows, towsCopy, towsClone})
+
+const townsObjs = {...tows}
+const townsObjsClone = {...townsObjs}
+
+townsObjsClone.test = "Test"
+
+console.log({townsObjs, townsObjsClone})
