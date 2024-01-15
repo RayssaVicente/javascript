14332a9aa667405b920934ca34ecad9dbeb7b9fc
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-15 09:01:28 -0300

    Exercício-16-Inicio

---
+++ exercício-16/index.js
@@ -0,0 +1,9 @@
+/*Você deverá criar uma função javascript que recebe como parâmetro a data de nascimento de uma pessoa no formato string e mostre no terminal a idade dessa pessoa no dia atual, sua próxima data de aniversário no formato “DD/MM/AAAA” e quantos dias faltam para o próximo aniversário dela. Para isso, você deverá utilizar o módulo dayjs, disponível no npm.
+
+Nessa aplicação você não utilizará o browser, você deverá utilizar o Node.js. Para executar seu arquivo com o Node.js basta acessar a pasta onde ele está localizado com o terminal e digitar o comando node seu_arquivo.js. Além disso, você deverá utilizar a sintaxe dos módulos CommonJS para trabalhar com o dayjs, ou seja, a função require().*/
+
+
+function data(){
+    const data = prompt("Digite seu data de nascimento: ")
+    return data
+}
