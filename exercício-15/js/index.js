b5f0c051e0ae4fb2fe9956f20f7c8037f90fe87d
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-15 08:10:41 -0300

    Exercício-15-Finalização

--- exercício-15/js/index.js
+++ exercício-15/js/index.js
@@ -6,44 +6,18 @@ Caso você não tenha os arquivos do projeto Calc.js você poderá baixá-los na
 
 import calculando from "./calculando.js"
 import copyToClipboard from "./copyToClipboard.js"
+import { heandleButtonPress, heandleClearButton, heandletype } from "./keysHandlers.js"
 import themeSwitcher from "./themeSwitcher.js"
 
-const input = document.getElementById('input')
-const resultInput = document.getElementById('result')
-
-const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
 document.querySelectorAll(".charKey").forEach(function (charKeyBtn){
-    charKeyBtn.addEventListener("click", function(){
-        const value = charKeyBtn.dataset.value
-        input.value += value
-    })
+    charKeyBtn.addEventListener("click", heandleButtonPress)
 })
 
-document.getElementById('clear').addEventListener('click', function(){
-    input.value = ""
-    input.focus()
-})
 
-input.addEventListener('keydown', function(ev) {
-    ev.preventDefault()
-    if (allowedKeys.includes(ev.key)){
-        input.value += ev.key
-        return
-    }
-
-    if (ev.key === 'Backspace'){
-        input.value = input.value.slice(0, -1)
-    }
-
-    if (ev.key === 'Enter'){
-        calculando()
-    }
-})
-
+document.getElementById('clear').addEventListener('click', heandleClearButton)
+document.getElementById('input').addEventListener('keydown', heandletype)
 document.getElementById('equal').addEventListener('click', calculando)
-
 document.getElementById('copyToClipboard').addEventListener('click', copyToClipboard)
-
 document.getElementById('themeSwitcher').addEventListener('click', themeSwitcher)
 
