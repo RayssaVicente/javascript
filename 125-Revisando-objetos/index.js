d7b1736aaaca0c2d20ae17787cca634a6f060053
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-19 08:14:13 -0300

    Aprendendo como funciona funções construtoras

--- 125-Revisando-objetos/index.js
+++ 125-Revisando-objetos/index.js
@@ -1,23 +1,38 @@
-const book = {
-    title: "Eragon",
-    pages: 468,
-    published: true,
-    inStock: 20,
-    tags: ["fantasy", "adventure", "medieval"],
-    autor:{
-        name: "Christopher Paolini"
-    },
-    addOnStock(quantity){
-        this.inStock += quantity
-    },
+// const book = {
+//     title: "Eragon",
+//     pages: 468,
+//     published: true,
+//     inStock: 20,
+//     tags: ["fantasy", "adventure", "medieval"],
+//     autor:{
+//         name: "Christopher Paolini"
+//     },
+//     addOnStock(quantity){
+//         this.inStock += quantity
+//     },
 
-}
+// }
+
 //função construtora
-
-console.log(book)
-book.addOnStock(50)
-
-book.save = function(){
-      // salva no banco de dados
+function Book(title, pages, tags, author){
+    this.title = title
+    this.pages = pages
+    this.tags = tags
+    this.author = author
+    this.published = false
+    this.inStock = 0
+    this.addOnStock = function addOnStock(quantity){
+        this.inStock += quantity
+    }
+    this.save = function(){
+        // salva no banco de dados
+  }
 }
-console.log(book)
+
+const author = {name: "Chiristopher Paolini"}
+const tags =  ["fantasy", "adventure", "medieval"]
+
+const eragon = new Book("Eragon", 468, tags, author)
+
+console.log(eragon)
+
