1b1bc5948349ed1352b1174d8fbd1c238c45d573
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-23 08:13:09 -0300

    Exercício-18 - Finalização

--- exercício-18/product.js
+++ exercício-18/product.js
@@ -3,10 +3,22 @@ class product {
         this.name = name
         this.description = description
         this.price = price
-        this.inStock = inStock
+        this.inStock = 0
     }
 
     addToStock(quantities){
-        
+        this.inStock += quantities
+    }
+
+    calculateDiscount(porcentagem){
+        return this.price *((100 - porcentagem)/ 100)
     }
 }
+
+const watch =  new product("Relogio de Pulso", "....", 80)
+watch.addToStock(99)
+const pricewithDiscount = watch.calculateDiscount(15)
+
+console.log(watch)
+console.log(pricewithDiscount)
+console.log(watch.calculateDiscount(10))
