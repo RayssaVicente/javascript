75ec6c8a0da94fb8e4b5e6c4dfa7f577fc748312
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-23 08:45:21 -0300

    Exercício-19 - Continuação

--- exercício-19/author.js
+++ exercício-19/author.js
@@ -1,5 +1,16 @@
+const post = require("./post")
+
 class author {
     constructor(name){
-        
+        this.name = name
+        this.posts = []
+    }
+
+    writePost(title, body){
+        const post = new post(title, body, this)
+        this.posts.push(post)
+        return post
     }
 }
+
+module.exportes = author
