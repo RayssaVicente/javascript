8f60efa1ceb4e8119586c4fa5fdd83157432d0a6
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-24 06:23:43 -0300

    Herança

---
+++ 134-Herança/index.js
@@ -0,0 +1,11 @@
+class Property {
+    constructor(area, price){
+        this.area = area
+        this.price = price
+    }
+
+    getPricePerSquareMeter(){
+        return this.price / this.area
+    }
+}
+
