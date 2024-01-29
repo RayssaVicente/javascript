a718ac3f692e29b993a712554ea0e854987943e9
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-29 06:53:15 -0300

    Exercício-20 - Algumas alterações

--- exercício-20/index.js
+++ exercício-20/index.js
@@ -15,5 +15,25 @@ import {Label} from "./Label.js"
 
 const title = new Component("h1", "body", {textContent: 'Olá, mundo!'})
 
-
+console.log()
 title.render()
+
+title.tag = "h3"
+title.build().render()
+
+const form = new Form('body')
+
+const label =  new Label('Nome: ', form, {htmlFor: 'nameInput'})
+const input = new Input(form, {id: 'nameInput', name: 'name'})
+
+form.render()
+label.render()
+
+form.addChildren(input)
+
+form.addChildren(
+    new Component('br'),
+    new Component('br'),
+    new Label('Data de nascimento: ', form, {htmlFor: 'birthdayInput'}),
+    new Input(form, {id: 'birthdayInput', name: 'birthday', type: 'date'})
+)
