796a4a677c3f8a7b9c3f01cc0bdc3807aab066e2
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-01-29 09:18:44 -0300

    Exercício-21-Começo

---
+++ exercício-21/Character.js
@@ -0,0 +1,13 @@
+class Character{
+    constructor(nome, pontosDeVida, pontosDeAtaque, PontosDeDefesa){
+        this.nome = nome
+        this.pontosDeVida = pontosDeVida
+        this.pontosDeAtaque = pontosDeAtaque
+        this.PontosDeDefesa = PontosDeDefesa
+    }
+
+    atacar(){
+        
+    }
+}
+    
