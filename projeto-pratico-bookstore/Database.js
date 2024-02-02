445739ff56a6469ccfe33b4928a43dbef978fe10
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-02-02 07:50:51 -0300

    Continuação

--- projeto-pratico-bookstore/Database.js
+++ projeto-pratico-bookstore/Database.js
@@ -15,25 +15,66 @@ module.exports = class Database {
         this.#storege.authors.push(author)
     }
 
-    findBooByName (bookName){
+    findBookByName (bookName){
         return this.#storege.books.find(b => b.name === bookName)
     }
 
     saveBook(book){
         const bookExists = this.findBooByName(book.name)
-        if(bookExists){
+        if(!bookExists){
             this.#storege.books.push(book)
         }
     }
 
     addBooksToStock(bookName, quantity){
-        const book = this.findBooByName(bookName)
+        const book = this.findBookByName(bookName)
         book?.addBooksToStock(quantity)
 
     }
 
     removeBooksFromStock(bookName, quantity){
-        const book = this.findBooByName(bookName)
+        const book = this.findBookByName(bookName)
         book?.removeBooksFromStock(quantity)
+    }
+
+    findPosterByName (posterName){
+        return this.#storege.posters.find(p => p.name === posterName)
+    }
+
+    savePoster(book){
+        const PosterExists = this.findPosterByName(poster.name)
+        if(!posterExists){
+            this.#storege.posters.push(poster)
+        }
+    }
+
+    addPostersToStock(posterName, quantity){
+        const poster = this.findPosterByNamposter(posterName)
+        poster?.addBooksToStock(quantity)
+
+    }
+
+    removePostersFromStock(posterName, quantity){
+        const poster = this.findPosterByName(posterName)
+        poster?.removeBooksFromStock(quantity)
+    }
+
+    saveUser(user){
+        const userExists = this.#storege.users.find(u => u.email === user.email)
+        if (!userExists){
+            this.#storege.users.push(user)
+        }
+    }
+
+    saveOrder(order){
+        this.#storege.orders.push(order)
+    }
+
+    showStorege(){
+        console.table(this.#storege.authors)
+        console.table(this.#storege.books)
+        console.table(this.#storege.posters)
+        console.table(this.#storege.users)
+        console.table(this.#storege.orders.map(order => order.data))
     }
 }
