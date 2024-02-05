62adb40c2c82f569c3d0a10f832a8018a14f0f35
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-02-05 09:06:44 -0300

    Atualização

---
+++ exercício-22/Loan.js
@@ -0,0 +1,23 @@
+const Installment = require("./installment")
+
+module.exports = class  Loan {
+    static #fee = 1.05
+
+    constructor(value, installment){
+        this.value = value
+        this.installment = []
+        for (let i = 1; i <= installment; i++){
+            this.installment.push(new Installment(value * Loan.#fee) / installment, i)
+        }
+        this.createdAt  = new Date()
+    }
+
+
+    static get fee(){
+        return Loan.#fee
+    }
+
+    static set fee(newFeePercentage){
+        Loan.#fee = 1  + (newFeePercentage /  100)
+    }
+}
