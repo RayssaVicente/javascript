75b361e17c16521a142d1522e981c70b2df7221e
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-02-14 12:09:25 -0300

    Aprendendo um pouco mais

--- 152-Tratamento-erro/index.js
+++ 152-Tratamento-erro/index.js
@@ -9,6 +9,16 @@ function sum (a, b){
     return firstNumber + secondNumber
 }
 
-console.log(sum(2, 9))
-console.log(sum(true, 14))
-console.log(sum(undefined, 22))
+try{
+    console.log(sum(2, 9))
+    console.log(sum(true, 14))
+    console.log(sum(undefined, 22))
+    console.log(sum(18, "8"))
+    console.log(sum(39, null))
+    console.log(sum(13, "zero"))
+} catch(error){
+    console.log("An error ocurred!")
+    console.log(error.message)
+}finally {
+    console.log("Calculando finished!")
+}
