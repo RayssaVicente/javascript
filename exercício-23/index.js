98c66a69ce632fc6b5bea82d467e25a6327f95e1
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-02-18 09:47:08 -0300

    Continuação

--- exercício-23/index.js
+++ exercício-23/index.js
@@ -1,3 +1,22 @@
-function validateEmail (){
-     
+const { error } = require("cli")
+
+function validateEmail (email){
+     if(!email.match(/(^\w{2,}@[a-zA-Z-0-9 ]{2,}.\w{2,})/)){
+        const error = new error ("Email invalido")
+        error.input = 'email'
+        throw error
+     }
 }
+
+function validatePassword (password){
+    if( password.length < 8 ||
+        password[/a-z/] ||
+        password[/A-Z/] ||
+        password[/0-9/] ||
+        password[/a-z-A-Z-0-9/s]){
+       const error =  new error ('Senha invalida')
+       error.input = 'password'
+       throw error
+    }
+}
+
