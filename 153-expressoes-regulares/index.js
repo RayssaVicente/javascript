881e9a717a18f7349029780c3b0a62b485a8580d
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-02-18 08:26:45 -0300

    finalização

--- 153-expressoes-regulares/index.js
+++ 153-expressoes-regulares/index.js
@@ -1,6 +1,8 @@
 function PhoneNumber (PhoneNumberString){
     const fixedString = PhoneNumberString.replace(/[\sa-zA-Z]/g, "")
-    this.contryCode = fixedString.match(/(?<=\+)\d(1,3)/)
+    this.contryCode = fixedString.match(/(?<=\+)\d{1,3}/)[0]
+    this.ddd = fixedString.match(/(?<=\()\d+(?=\))/)[0]
+    this.number = fixedString.match(/(?<=\)).+/)[0].replace(/-/g, "")
 }
 
 console.log(new PhoneNumber('+55 (22) 9 9876-5432'))
