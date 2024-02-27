ab0d2be12158ad19af65b5a743a7926c7af71d4d
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-02-27 07:27:22 -0300

    Continuação

--- exercício-23/index.js
+++ exercício-23/index.js
@@ -1,12 +1,42 @@
 const { error } = require("cli")
 
 function validateEmail (email){
-     if(!email.match(/(^\w{2,}@[a-zA-Z-0-9 ]{2,}.\w{2,})/)){
-        const error = new error ("Email invalido")
-        error.input = 'email'
-        throw error
+     if(!email.match(/\w{2,}@[a-zA-Z]{2,}.[a-zA-Z]{2,}/)){
+        const err = new err ("Email invalido")
+        err.input = 'email'
+        throw err
      }
 }
+
+function resetFormStyles(){
+    Object.entries(userInputs).forEach(([key, value]) => {
+        value.classList.remove('success', 'error')
+        document.querySelector(`#$(key).error`).textContent = ""
+    }) 
+
+    
+}
+
+const userInputs = {
+    name: document.querySelector('#name'),
+    email: document.querySelector('#email'),
+    password: document.querySelector('#password')
+}
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (ev) =>{
+    ev.preventDefault
+
+    try{
+        userInputs.name.classList.add('success')
+        validateEmail(userInputs.email.value)
+        userInputs.email.classList.add('success')
+    } catch (err){
+        userInputs[err.input].classList.add('error')
+        document.querySelector(`#$(err.input).error`).textContent = err.mensage
+    }
+})
 
 function validatePassword (password){
     if( password.length < 8 ||
