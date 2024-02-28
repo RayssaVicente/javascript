513c3999a85c522aa47e57a5221230aba4b1e31b
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-02-28 07:21:56 -0300

    Forma mais comum de usar Promises

--- 156-Conhecendo-promises/index.js
+++ 156-Conhecendo-promises/index.js
@@ -1,22 +1,42 @@
-// Pendente - pending
-// Resolvida - resolved
-// Rejeitada - rejected
+ // Pendente - pending
+ // Resolvida - resolved
+ // Rejeitada - rejected
 
-const p = new Promise((resolve, reject)=>{
-    console.log('A promise está sendo executada.')
-    setTimeout(()=>{
-        if (true){
-            reject(false)
-        }
-        console.log('Resolvendo a promise...')
-        resolve(true)
-    }, 1000 * 2)
+// const p = new Promise((resolve, reject)=>{
+//     console.log('A promise está sendo executada.')
+//     setTimeout(()=>{
+//         if (true){
+//             reject(false)
+//         }
+//         console.log('Resolvendo a promise...')
+//         resolve(true)
+//     }, 1000 * 2)
     
-})
+// })
+
+// console.log(p)
+
+// setTimeout(()=>{
+//     console.log(p)
+}, 1000 * 3)
+
+
+
+function execute(){
+    return new Promise((resolve, reject)=>{
+        console.log('A promise está sendo executada...')
+        setTimeout(()=> {
+            console.log('Resolvendo a promise...')
+            resolve('Resultado')
+        }, 1000)
+    })
+}
+
+const p = execute()
 
 console.log(p)
 
-setTimeout(()=>{
+setTimeout(()=> {
     console.log(p)
-}, 1000 * 3)
+}, 2000)
 
