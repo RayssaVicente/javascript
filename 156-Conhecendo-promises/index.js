2d7fe417b22ab2b9025a72b9040e0cbb89777af5
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-02-28 07:41:03 -0300

    Utilizando then e catch

--- 156-Conhecendo-promises/index.js
+++ 156-Conhecendo-promises/index.js
@@ -2,6 +2,8 @@
  // Resolvida - resolved
  // Rejeitada - rejected
 
+const { result } = require("lodash")
+
 // const p = new Promise((resolve, reject)=>{
 //     console.log('A promise está sendo executada.')
 //     setTimeout(()=>{
@@ -26,9 +28,13 @@ function execute(){
     return new Promise((resolve, reject)=>{
         console.log('A promise está sendo executada...')
         setTimeout(()=> {
+           if(true){
+            reject('a promise foi rejeitada')
+           }else {
             console.log('Resolvendo a promise...')
             resolve('Resultado')
-        }, 1000)
+           }
+        }, 1000 * 2)
     })
 }
 
@@ -39,4 +45,11 @@ console.log(p)
 setTimeout(()=> {
     console.log(p)
 }, 2000)
+
+p.then((result)=>{
+    console.log(`A promise foi resolvida. O resultado foi: ${result}`)
+}).catch((err)=>{
+    console.log(`A promise foi rejeitada! Motivo: ${err}`)
+})
+
 
