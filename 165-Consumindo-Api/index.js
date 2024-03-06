4e5fb77f0e47a0e588c08e0dc88b9d96f249e8d1
Rayssa Vicente da Silva Viegas <rayssavicenteviegas@gmail.com>
2024-03-06 06:58:42 -0300

    Fazendo a base

---
+++ 165-Consumindo-Api/index.js
@@ -0,0 +1,58 @@
+let transaction = []
+
+function createTransactionContainer(id){
+    const container = document.createElement('div')
+    container.classList.add('transaction')
+    container.id = `transaction-${id}`
+    return container
+}
+
+function createTransactionTitle(name){
+    const title = document.createElement('span')
+    title.classList.add('transaction-title')
+    title.textContent = name
+    return title
+}
+
+function createTransactionAmount(amount){
+    const span = document.createElement('span')
+
+    const formater = Intl.NumberFormat('pt-BR', {
+        compactDisplay: 'long',
+        currency: 'BRL',
+        style: 'currency'
+    })
+    const formatedAmount = formater.format(amount)
+
+    if(amount > 0){
+        span.textContent = `${formatedAmount} C`
+        span.classList.add('credit')
+    }else{
+        span.textContent = `${formatedAmount} D`
+        span.classList.add('debit')
+    }
+
+    return span
+}
+
+function renderTransaction(transaction){
+    const container = createTransactionContainer(transaction.id)
+    const title = createTransactionTitle(transaction.name)
+    const amount = createTransactionAmount(transaction.amount)
+
+    container.append(title, amount)
+    document.querySelector('#transaction').append(container)
+}
+
+async function fetchTransaction(){
+    return await fetch("http://localhost:3000/transactions").then(res => res.json())
+}
+
+async function setup(){
+    const results = await fetchTransaction()
+    transaction.push(...results)
+    transaction.forEach(renderTransaction)
+}
+
+document.addEventListener('DOMContentLoaded', setup)
+
